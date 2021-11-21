c0686bbb4688563633b3cc4a831d5adaae49adf0
alaa-hany <62419181+Alaa-Hany@users.noreply.github.com>
2021-11-22 01:57:57 +0200

    Update get_versions_info.php

--- get_versions_info.php
+++ get_versions_info.php
@@ -1,18 +1,14 @@
 <?php
-// Facebook ==> 284882215
-// whatsapp ==> 284882215
-// snapchat ==> 447188370
-
-if($argc < 2){
-    die("usage:\nphp ". basename(__FILE__)."  id \nexample : php " .  basename(__FILE__) . " 284882215 // FaceBook\n" );
+function getIpaVersionsWithId( $id ){
+    $url = "https://tools.lancely.tech/apple/app-version/US/" . $argv[1];
+    $pattern = "/appVersion:(.*)\}\],\s?/s" ;
+    $allDataAsJson = Null ;
+    if(preg_match($pattern,  file_get_contents($url) , $matches) and (!empty($matches[1]))){
+        $patterns = [  '/\s/',  '/:([\w\$]+),/'  ,  '/:([\w\$]+)\}/' , '/\{(\w+):/', '/,(\w+):/'];
+        $replacement = [ '' , ':"$1",' , ':"$1"}'  , '{"$1":' , ',"$1":'];
+        $allDataAsJson = '{"data" : ' .preg_replace( $patterns, $replacement, trim($matches[1]) ) . "}" ;
+    }    
+    
+    return json_decode( $allDataAsJson  , true);
 }
-
-$url = "https://tools.lancely.tech/apple/app-version/US/" . $argv[1];
-$pattern = "/appVersion:(.*)\}\],\s?/s" ;
-$allDataAsJson = Null ;
-if(preg_match($pattern,  file_get_contents($url) , $matches) and (!empty($matches[1]))){
-    $patterns = [  '/\s/',  '/:([\w\$]+),/'  ,  '/:([\w\$]+)\}/' , '/\{(\w+):/', '/,(\w+):/'];
-    $replacement = [ '' , ':"$1",' , ':"$1"}'  , '{"$1":' , ',"$1":'];
-    $allDataAsJson = '{"data" : ' .preg_replace( $patterns, $replacement, trim($matches[1]) ) . "}" ;
-}
-var_dump($allDataAsJson);
+    
